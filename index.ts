0c64a172d287f7c62ff1523bf2353d4eaa619a1c
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2023-10-18 15:56:49 +0200

    basic router structure created

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import WebSocket from 'ws';
 
 const app = express();
 
@@ -9,6 +8,6 @@ app.get('/', (req, res) => {
 
 
 app.listen(3000, 'localhost', () => {
-    console.log("Server is listening on port 3000");
+    console.log("Server is listening on port: 3000");
     
 });
