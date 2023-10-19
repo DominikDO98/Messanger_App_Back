c3c5022b02e895180dc9fd2a4e109ae78e8fdaf1
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2023-10-19 14:13:04 +0200

    websocket updated

--- index.ts
+++ index.ts
@@ -3,13 +3,13 @@ import { homeRouter } from './routes/home.router';
 import { authRouter } from './routes/auth.router';
 
 import { app } from './utlis/app'
-import { socket } from './utlis/websocket'
+import { server } from './utlis/websocket'
 
 app.use(express.json());
 
 app.use('/', homeRouter);
 app.use("/auth", authRouter);
 
-socket.listen(3000, 'localhost', () => {
+server.listen(3000, 'localhost', () => {
     console.log("Server is listening on port: 3000");
 });
--- utlis/websocket.ts
+++ utlis/websocket.ts
@@ -1,18 +1,25 @@
 import WebSocket from 'ws';
 import {createServer} from 'http';
 import { app } from './app';
+import path from 'path'
 
-export const socket = createServer(app)
-const wsServer = new WebSocket.Server({ server: socket})
+const socket = app.get('/ws', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
+})
 
-wsServer.on('connection', (ws) => {
+export const server = createServer(socket)
+const wss = new WebSocket.Server({ server: server})
+
+wss.on('connection', (ws) => {
     console.log('A new client connected');
     ws.send('Welcome New Client');
 
     ws.on('message', (message) => {
         console.log('recived msg: ', String(message));
 
-        ws.send(message);
+        wss.clients.forEach((client) => {
+            client.send(message);
+        });
         
     })
     
