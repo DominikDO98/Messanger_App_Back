ad447b3a7edf2348d696498a998d60b7cdd2d551
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2023-10-19 12:45:11 +0200

    basic socket creatd in index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,32 @@
 import express from 'express';
 import { homeRouter } from './routes/home.router';
 import { authRouter } from './routes/auth.router';
+import WebSocket, { WebSocketServer } from 'ws';
+import {createServer} from 'http';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/', homeRouter);
-app.use("/auth", authRouter)
+app.use("/auth", authRouter);
 
+const socket = createServer(app)
+const wsServer = new WebSocket.Server({ server: socket})
 
-app.listen(3000, 'localhost', () => {
-    console.log("Server is listening on port: 3000");
+wsServer.on('connection', (ws) => {
+    console.log('A new client connected');
+    ws.send('Welcome New Client');
+
+    ws.on('message', (message) => {
+        console.log('recived msg: ', String(message));
+
+        ws.send(message);
+        
+    })
     
+})
+
+socket.listen(3000, 'localhost', () => {
+    console.log("Server is listening on port: 3000");
 });
