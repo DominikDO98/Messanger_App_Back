bbca2ad8c6b19442b9546de2de0004324df65859
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2023-10-23 10:16:46 +0200

    sendMessage method added

--- repository/messages.repository.ts
+++ repository/messages.repository.ts
@@ -14,7 +14,7 @@ export class MessegesRepository {
     }
 
     static async loadMessages (room_id: string): Promise<Partial<TMessage[]> | null> {
-        const [ results ] = await pool.execute('SELECT `message_text`, `created_at`, `messege_id`, `from_user_id` FROM `messeges` WHERE `to_room_id` = :room_id LIMIT 50 ORDER BY `created_at` DESC', {
+        const [ results ] = await pool.execute('SELECT `message_text`, `created_at`, `messege_id`, `from_user_id` FROM `messeges` WHERE `to_room_id` = :room_id ORDER BY `created_at` ASC LIMIT 50', {
             room_id: room_id,
         });
 
@@ -22,9 +22,22 @@ export class MessegesRepository {
         return results[0]
         
     }
+    static async sendMessage (message: Omit<TMessage, 'created_at'>): Promise<string> {
+            await pool.execute('INSERT INTO `messages` (`message_id`, `from_user_id`, `to_room_id`, `message_text`) VALUES (:message_id, :user_id, :room_id, :message_text)', {
+                message_id: message.message_id,
+                user_id: message.from_user_id,
+                room_id: message.to_room_id,
+                message_text: message.message_text,
+            })
+            
+            const [results] = await pool.execute('SELECT `message_id`, `to_room_id` FROM `messages` WHERE `message_id` = :id', {
+                id: message.message_id,
+            })
+            console.log(results[0]);
+            
+            return results[0]
+        
+    }
 
     
 }
-
-
-MessegesRepository.loadMessages('1');
